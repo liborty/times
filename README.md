546cff8b3c7dfa963540c14e5ec3182aa2cf6171
L <457124+liborty@users.noreply.github.com>
2022-07-16 14:50:59 +1000

    v 0.1.5

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# Times
+# Times: Benchmark for Timing and Comparing Algorithms in Rust
 
 [<img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/liborty/times/HEAD?logo=github">](https://github.com/liborty/times)
 [<img alt="crates.io" src="https://img.shields.io/crates/v/times?logo=rust">](https://crates.io/crates/times)
 [<img alt="crates.io" src="https://img.shields.io/crates/d/times?logo=rust">](https://crates.io/crates/times)
 [<img alt="docs.rs" src="https://img.shields.io/docsrs/times?logo=rust">](https://docs.rs/times)
-
-# Times: Benchmark for Timing and Comparing Algorithms in Rust
 
 ## Usage
 
@@ -15,37 +13,52 @@ use times::*;
 
 ## Introduction
 
-This crate will be typically included under `[dev-dependecies]` in `Cargo.toml` and then used by source files under `tests` or `benches` directories. Whenever the runtime speed comparisons are of interest, which they are practically always.
+This crate will be typically added in `Cargo.toml` under `[dev-dependecies]`  and then used by source files under `tests` or `benches` directories. To be used whenever the runtime speed comparisons are of interest, which is practically always.
 
-Suitable for testing algorithms that work on a whole `Vec` of data, for example sort. The correctness of the results has to be tested separately. Here the results are thrown away and only the execution time is measured.
+Is suitable for testing algorithms that work on a whole `Vec` of data, for example sort. The correctness of the results
+should be tested separately. Here the results are thrown away and only the execution time is measured.
 
-Random data are automatically generated using `ran` crate and then algorithms from an array of closures are repeatedly run, timed and their statistics are collected (arithmetic mean execution time and its standard deviation). This will average out the temporary effects of changing machine load, and changing data. All the algorithms are run over the same data but the data is changed for each repeat.
+Random data are automatically generated using `ran` crate and then algorithms from a given array of closures are repeatedly run and their statistics are collected (arithmetic mean of the execution *times* and its standard error). The repeated runs  average out the temporary effects of changing machine load and changing data. All the algorithms are run over the same data but the data is changed for each repeat run.
 
-The standard deviation is here an inverse measure of the timing reliability. The reliability will be improved by increasing the number of runs. The same as when performing  any measurements.
+Standard error (`ste`) estimates the doubt about the accuracy of any (repeated) measurements. Thus high `ste` means poor accuracy. Accuracy can generally be increased  by increasing the number of repeats. Here the extraneous influence of the machine load is also reduced as the length of the data vectors increases.
 
-The tests are also automatically repeated over different lengths of the input data vectors, in steps of their *orders of magnitude*: 10,100,1000,10000, etc.. This makes it easy to compare the algorithms' rankings of merit, as the difficulty of the problem increases. The ones with lower overall computational complexity and/or faster implementations, will start convincingly winning.
+We generate new random data for each repeated run, so the differences in `ste` between algorithms inform us about the their relative stability under changing data. Some algorithms can suffer from data sensitivity (poor worst-case performance) and this may be indicated by relatively high `ste`.
+
+The tests are also automatically repeated over different lengths of the input data vectors, in steps of their *orders of magnitude*: 10,100,1000,10000, etc. This is to enable comparisons of the algorithms' rankings of merit, as the difficulty of the problem increases. The algorithms with lower computational complexity and/or faster implementations will start convincingly winning. 
+
+A word of warning: it is not recommended to set the magnitudes range to more than 5, as it may take a long time to run and the process may have to be externally terminated. Depending, of course, on the algorithms and the speed of the machine.
 
 ## Main Features
 
-* Ease of Use - just specify as arguments:
-  * the type of the random test data generator,
-  * the range of magnitudes (zeros),
-  * the number of repeats,
-  * some arbitrary names to identify the algorithms by
-  * the closures invoking the algorithms (in the same order as their names).
+* Ease of Use - just specify:
+  * the type of the random test data,
+  * the range of magnitudes of data vectors lengths,
+  * the number of repeats with different data,
+  * some labels/names to identify the algorithms by
+  * the closures invoking the algorithms (in the same order as their labels).
 
-The algorithms are automatically sorted by their execution times, separately within each magnitude of data category. In other words, the fastest algorithm in each data category will be always listed first and the slowest last.
+* Sorted output. The algorithms are automatically sorted by their execution times within each magnitude of data category. In other words, the fastest algorithm in each data category will be listed first and the slowest last.
 
-So far here are three functions implemented for testing algorithms on three end-types of data: `benchu8, benchu64, benchf64`. Plus their mutable versions for testing mutable algorithms: `mutbenchu8, mutbenchu64, mutbenchf64`
+## Provided Testing Functions
 
-This can be extended just by copying the code of one of these functions and changing the concrete type therein.
+* Functions for testing algorithms on vectors of three different end-types of data: `benchu8, benchu64, benchf64`. 
+
+* Mutable versions for testing mutable algorithms: `mutbenchu8, mutbenchu64, mutbenchf64`. A mutable version has to be used even when just one of the tested algorithms mutates its input.
+
+ * Versions for algorithms working on n-dimensional data (matrices): `benchvvu8, benchvvu64 and benchvvf64`.
+
+ Other end-types may be included later.
 
 ## Conclusion
 
-Please see `tests/tests.rs` for an example application. From the benchmarks we conclude that the fastest standard Rust destructive sort, named here `mutsort`, is indeed the fastest but only up to about 100 items to be sorted. Then all three versions of my `hashsort` overtake it. The merit order of the algorithms then remains fixed from about 1000 upwards.
+Please see `tests/test.rs` for examples of how to specify the closures and call these functions on them.
+
+These benchmarks show that the fastest standard Rust destructive sort, labelled as `mutsort` here, is indeed the fastest but only up to about 100 sort items. Then all three versions of my `hashsort` overtake it.
 
 ## Appendix - Recent Releases
 
-**Version 0.1.4** Added `benchvvu8` for closures acting on (unmutable) `Vec<Vec<u8>>` and similarly `benchvvf64` for `Vec<Vec<f64>>`.
+**Version 0.1.5** Corrected the report headlines. Introduced standard errors. Added `benchvvu64`.
 
-**Version 0.1.3** Benchmark functions for `Vec` data types: u8,u64,f64, in plain and mutable varieties.
+**Version 0.1.4** Added `benchvvu8` for closures acting on (immutable) `Vec<Vec<u8>>` and similarly `benchvvf64` for `Vec<Vec<f64>>`.
+
+**Version 0.1.3** Benchmark functions for `Vec` data types: u8,u64,f64, in plain and mutable varieties
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,20 @@ use devtimer::{DevTime};
 use indxvec::{Indices, printing::*, Vecops};
 use ran::*;
 
+fn report(times:&[u128],timessq:&[u128],names:&[&str],repeats:f64) {
+    let timesx = times.mergesort_indexed();
+    let times_sorted = timesx.unindex(times,true);
+    let names_sorted = timesx.unindex(names,true);
+    let timessq_sorted = timesx.unindex(timessq,true);
+    for i in 0..names.len() {
+        let sx2 = timessq_sorted[i] as f64;
+        let sx = times_sorted[i] as f64;
+        // standard error as a percentage of the mean
+        let ste = 100.0*(repeats*sx2/(sx*sx) - 1.).sqrt();
+        println!("{MG}{:<18}{GR}{:>10.0} ± {:>3.2}%{UN}",names_sorted[i],sx/repeats,ste);
+    }
+}
+
 /// Repeated tests on vectors of magnitudes steps in length, e.g. 10,100,1000  
 /// Time n runs of listed closures, identified by names,
 /// on random data of type specified by rn, e.g. rn = Rnum::newu64;
@@ -16,15 +30,14 @@ pub fn mutbenchu64(
     closures:&[fn(&mut[u64])]) { // concrete type here
 
     let algno = names.len();
-    let rint = repeats as u128;
     let mut timer = DevTime::new_simple();
 
-    println!("\n{YL}Timing sort algorithms on u64 data (in nanoseconds){UN}");  
- 
+    println!("\n{YL}Nanoseconds of {BL}{}{YL} algorithms, {BL}&mut[u64]{YL} data, {BL}{}{YL} magnitudes, {BL}{}{YL} repeats:{UN}",
+        algno,magnitudes,repeats );  
+
     for mag in 1..magnitudes+1 {  
         let d = 10_usize.pow(mag as u32);      
-        println!("\nTesting sorts on a set of {GR}{}{UN} random vectors\nof length {GR}{}{UN} each:\n",
-            repeats,d);
+        println!("\nDimensions: {BL}{}{UN}\n",d);
         let mut times = vec![0_u128;algno];
         let mut timessq = vec![0_u128;algno];
         for _ in 0..repeats {
@@ -38,16 +51,8 @@ pub fn mutbenchu64(
                 times[i] += this_time; 
                 timessq[i] += this_time*this_time; 
             }
-        }   
-        let timesx = times.mergesort_indexed();
-        let times_sorted = timesx.unindex(&times,true);
-        let names_sorted = timesx.unindex(names,true);
-        let timessq_sorted = timesx.unindex(&timessq,true);
-        
-        for i in 0..names.len() {
-            println!("{MG}{:<18}{GR}{:>10.0} ± {:>8.0}{UN}",names_sorted[i],times_sorted[i]/rint,
-            (((timessq_sorted[i]-times_sorted[i]*times_sorted[i]/rint)/rint) as f64).sqrt()); 
-        } 
+        }
+        report(&times,&timessq,names,repeats as f64)
     }
 }
 
@@ -63,15 +68,14 @@ pub fn mutbenchf64(
     closures:&[fn(&mut[f64])]) { // concrete type here
 
     let algno = names.len();
-    let rint = repeats as u128;
     let mut timer = DevTime::new_simple();
 
-    println!("\n{YL}Timing sort algorithms on f64 data (in nanoseconds){UN}");  
+    println!("\n{YL}Nanoseconds of {BL}{}{YL} algorithms, {BL}&mut[f64]{YL} data, {BL}{}{YL} magnitudes, {BL}{}{YL} repeats:{UN}",
+        algno,magnitudes,repeats);
  
     for mag in 1..magnitudes+1 {  
-        let d = 10_usize.pow(mag as u32);      
-        println!("\nTesting sorts on a set of {GR}{}{UN} random vectors\nof length {GR}{}{UN} each:\n",
-            repeats,d);
+        let d = 10_usize.pow(mag as u32);
+        println!("\nDimensions: {BL}{}{UN}\n",d);
         let mut times = vec![0_u128;algno];
         let mut timessq = vec![0_u128;algno];
         for _ in 0..repeats {
@@ -85,18 +89,11 @@ pub fn mutbenchf64(
                 times[i] += this_time; 
                 timessq[i] += this_time*this_time; 
             }
-        }   
-        let timesx = times.mergesort_indexed();
-        let times_sorted = timesx.unindex(&times,true);
-        let names_sorted = timesx.unindex(names,true);
-        let timessq_sorted = timesx.unindex(&timessq,true);
-        
-        for i in 0..names.len() {
-            println!("{MG}{:<18}{GR}{:>10.0} ± {:>8.0}{UN}",names_sorted[i],times_sorted[i]/rint,
-            (((timessq_sorted[i]-times_sorted[i]*times_sorted[i]/rint)/rint) as f64).sqrt()); 
-        } 
+        }
+        report(&times,&timessq,names,repeats as f64)
     }
 }
+
 
 /// Repeated tests on vectors of magnitudes steps in length, e.g. 10,100,1000  
 /// Time n runs of listed closures, identified by names,
@@ -110,15 +107,14 @@ pub fn mutbenchu8(
     closures:&[fn(&mut[u8])]) { // concrete type here
 
     let algno = names.len();
-    let rint = repeats as u128;
     let mut timer = DevTime::new_simple();
 
-    println!("\n{YL}Timing sort algorithms on u8 data (in nanoseconds){UN}");  
+    println!("\n{YL}Nanoseconds of {BL}{}{YL} algorithms, {BL}&mut[u8]{YL} data, {BL}{}{YL} magnitudes, {BL}{}{YL} repeats:{UN}",
+        algno,magnitudes,repeats);
  
     for mag in 1..magnitudes+1 {  
-        let d = 10_usize.pow(mag as u32);      
-        println!("\nTesting sorts on a set of {GR}{}{UN} random vectors\nof length {GR}{}{UN} each:\n",
-            repeats,d);
+        let d = 10_usize.pow(mag as u32); 
+        println!("\nDimensions: {BL}{}{UN}\n",d);     
         let mut times = vec![0_u128;algno];
         let mut timessq = vec![0_u128;algno];
         for _ in 0..repeats {
@@ -133,15 +129,7 @@ pub fn mutbenchu8(
                 timessq[i] += this_time*this_time; 
             }
         }   
-        let timesx = times.mergesort_indexed();
-        let times_sorted = timesx.unindex(&times,true);
-        let names_sorted = timesx.unindex(names,true);
-        let timessq_sorted = timesx.unindex(&timessq,true);
-        
-        for i in 0..names.len() {
-            println!("{MG}{:<18}{GR}{:>10.0} ± {:>8.0}{UN}",names_sorted[i],times_sorted[i]/rint,
-            (((timessq_sorted[i]-times_sorted[i]*times_sorted[i]/rint)/rint) as f64).sqrt()); 
-        } 
+        report(&times,&timessq,names,repeats as f64)
     }
 }
 
@@ -157,15 +145,14 @@ pub fn benchu64(
     closures:&[fn(&[u64])]) { // concrete type here
 
     let algno = names.len();
-    let rint = repeats as u128;
     let mut timer = DevTime::new_simple();
 
-    println!("\n{YL}Timing sort algorithms on u64 data (in nanoseconds){UN}");  
+    println!("\n{YL}Nanoseconds of {BL}{}{YL} algorithms, {BL}&[u64]{YL} data, {BL}{}{YL} magnitudes, {BL}{}{YL} repeats:{UN}",
+        algno,magnitudes,repeats );
  
     for mag in 1..magnitudes+1 {  
-        let d = 10_usize.pow(mag as u32);      
-        println!("\nTesting sorts on a set of {GR}{}{UN} random vectors\nof length {GR}{}{UN} each:\n",
-            repeats,d);
+        let d = 10_usize.pow(mag as u32);
+        println!("\nDimensions: {BL}{}{UN}\n",d);
         let mut times = vec![0_u128;algno];
         let mut timessq = vec![0_u128;algno];
         for _ in 0..repeats {
@@ -178,16 +165,8 @@ pub fn benchu64(
                 times[i] += this_time; 
                 timessq[i] += this_time*this_time; 
             }
-        }   
-        let timesx = times.mergesort_indexed();
-        let times_sorted = timesx.unindex(&times,true);
-        let names_sorted = timesx.unindex(names,true);
-        let timessq_sorted = timesx.unindex(&timessq,true);
-        
-        for i in 0..names.len() {
-            println!("{MG}{:<18}{GR}{:>10.0} ± {:>8.0}{UN}",names_sorted[i],times_sorted[i]/rint,
-            (((timessq_sorted[i]-times_sorted[i]*times_sorted[i]/rint)/rint) as f64).sqrt()); 
         } 
+        report(&times,&timessq,names,repeats as f64)
     }
 }
 
@@ -203,15 +182,14 @@ pub fn benchf64(
     closures:&[fn(&[f64])]) { // concrete type here
 
     let algno = names.len();
-    let rint = repeats as u128;
     let mut timer = DevTime::new_simple();
 
-    println!("\n{YL}Timing sort algorithms on f64 data (in nanoseconds){UN}");  
+    println!("\n{YL}Nanoseconds of {BL}{}{YL} algorithms, {BL}&[f64]{YL} data, {BL}{}{YL} magnitudes, {BL}{}{YL} repeats:{UN}",
+        algno,magnitudes,repeats );
  
     for mag in 1..magnitudes+1 {  
-        let d = 10_usize.pow(mag as u32);      
-        println!("\nTesting sorts on a set of {GR}{}{UN} random vectors\nof length {GR}{}{UN} each:\n",
-            repeats,d);
+        let d = 10_usize.pow(mag as u32);
+        println!("\nDimensions: {BL}{}{UN}\n",d);
         let mut times = vec![0_u128;algno];
         let mut timessq = vec![0_u128;algno];
         for _ in 0..repeats {
@@ -225,15 +203,7 @@ pub fn benchf64(
                 timessq[i] += this_time*this_time; 
             }
         }   
-        let timesx = times.mergesort_indexed();
-        let times_sorted = timesx.unindex(&times,true);
-        let names_sorted = timesx.unindex(names,true);
-        let timessq_sorted = timesx.unindex(&timessq,true);
-        
-        for i in 0..names.len() {
-            println!("{MG}{:<18}{GR}{:>10.0} ± {:>8.0}{UN}",names_sorted[i],times_sorted[i]/rint,
-            (((timessq_sorted[i]-times_sorted[i]*times_sorted[i]/rint)/rint) as f64).sqrt()); 
-        } 
+        report(&times,&timessq,names,repeats as f64)
     }
 }
 
@@ -249,15 +219,14 @@ pub fn benchu8(
     closures:&[fn(&[u8])]) { // concrete type here
 
     let algno = names.len();
-    let rint = repeats as u128;
     let mut timer = DevTime::new_simple();
 
-    println!("\n{YL}Timing sort algorithms on u8 data (in nanoseconds){UN}");  
- 
+    println!("\n{YL}Nanoseconds of {BL}{}{YL} algorithms, {BL}&[u8]{YL} data, {BL}{}{YL} magnitudes, {BL}{}{YL} repeats:{UN}",
+        algno,magnitudes,repeats );  
+
     for mag in 1..magnitudes+1 {  
-        let d = 10_usize.pow(mag as u32);      
-        println!("\nTesting sorts on a set of {GR}{}{UN} random vectors\nof length {GR}{}{UN} each:\n",
-            repeats,d);
+        let d = 10_usize.pow(mag as u32);
+        println!("\nDimensions: {BL}{}{UN}\n",d);
         let mut times = vec![0_u128;algno];
         let mut timessq = vec![0_u128;algno];
         for _ in 0..repeats {
@@ -271,15 +240,7 @@ pub fn benchu8(
                 timessq[i] += this_time*this_time; 
             }
         }   
-        let timesx = times.mergesort_indexed();
-        let times_sorted = timesx.unindex(&times,true);
-        let names_sorted = timesx.unindex(names,true);
-        let timessq_sorted = timesx.unindex(&timessq,true);
-        
-        for i in 0..names.len() {
-            println!("{MG}{:<18}{GR}{:>10.0} ± {:>8.0}{UN}",names_sorted[i],times_sorted[i]/rint,
-            (((timessq_sorted[i]-times_sorted[i]*times_sorted[i]/rint)/rint) as f64).sqrt()); 
-        } 
+        report(&times,&timessq,names,repeats as f64)
     }
 }
 
@@ -296,15 +257,14 @@ pub fn benchvvu8(
     closures:&[fn(&[Vec<u8>])]) { // concrete type here
 
     let algno = names.len();
-    let rint = repeats as u128;
     let mut timer = DevTime::new_simple();
 
-    println!("\n{YL}Timing sort algorithms on u8 data (in nanoseconds){UN}");  
+    println!("\n{YL}Nanoseconds of {BL}{}{YL} algorithms, {BL}&[Vec<u8>]{YL} data, {BL}{}{YL} magnitudes, {BL}{}{YL} points, {BL}{}{YL} repeats:{UN}",
+        algno,points,magnitudes,repeats );  
  
     for mag in 1..magnitudes+1 {  
-        let d = 10_usize.pow(mag as u32);      
-        println!("\nTesting sorts on a set of {GR}{}{UN} random vectors\nof length {GR}{}{UN} each:\n",
-            repeats,d);
+        let d = 10_usize.pow(mag as u32);
+        println!("\nDimensions: {BL}{}{UN}\n",d);
         let mut times = vec![0_u128;algno];
         let mut timessq = vec![0_u128;algno];
         for _ in 0..repeats {
@@ -318,21 +278,13 @@ pub fn benchvvu8(
                 timessq[i] += this_time*this_time; 
             }
         }   
-        let timesx = times.mergesort_indexed();
-        let times_sorted = timesx.unindex(&times,true);
-        let names_sorted = timesx.unindex(names,true);
-        let timessq_sorted = timesx.unindex(&timessq,true);
-        
-        for i in 0..names.len() {
-            println!("{MG}{:<18}{GR}{:>10.0} ± {:>8.0}{UN}",names_sorted[i],times_sorted[i]/rint,
-            (((timessq_sorted[i]-times_sorted[i]*times_sorted[i]/rint)/rint) as f64).sqrt()); 
-        } 
+        report(&times,&timessq,names,repeats as f64)
     }
 }
 
 /// Repeated tests on vectors of vectors of magnitudes steps in length, e.g. 10,100,1000  
 /// Time n runs of listed closures, identified by names,
-/// on random data of type specified by rn, e.g. rn = Rnum::newf64;
+/// on random data of type &[Vec<f64>] and rn = Rnum::newf64;
 /// identified by names listed in names.
 pub fn benchvvf64(
     rn:Rnum,
@@ -343,15 +295,14 @@ pub fn benchvvf64(
     closures:&[fn(&[Vec<f64>])]) { // concrete type here
 
     let algno = names.len();
-    let rint = repeats as u128;
     let mut timer = DevTime::new_simple();
 
-    println!("\n{YL}Timing sort algorithms on u8 data (in nanoseconds){UN}");  
- 
+    println!("\n{YL}Nanoseconds of {BL}{}{YL} algorithms, {BL}&[Vec<f64>]{YL} data, {BL}{}{YL} magnitudes, {BL}{}{YL} points, {BL}{}{YL} repeats:{UN}",
+        algno,points,magnitudes,repeats ); 
+
     for mag in 1..magnitudes+1 {  
-        let d = 10_usize.pow(mag as u32);      
-        println!("\nTesting sorts on a set of {GR}{}{UN} random vectors\nof length {GR}{}{UN} each:\n",
-            repeats,d);
+        let d = 10_usize.pow(mag as u32);
+        println!("\nDimensions: {BL}{}{UN}\n",d); 
         let mut times = vec![0_u128;algno];
         let mut timessq = vec![0_u128;algno];
         for _ in 0..repeats {
@@ -365,14 +316,44 @@ pub fn benchvvf64(
                 timessq[i] += this_time*this_time; 
             }
         }   
-        let timesx = times.mergesort_indexed();
-        let times_sorted = timesx.unindex(&times,true);
-        let names_sorted = timesx.unindex(names,true);
-        let timessq_sorted = timesx.unindex(&timessq,true);
-        
-        for i in 0..names.len() {
-            println!("{MG}{:<18}{GR}{:>10.0} ± {:>8.0}{UN}",names_sorted[i],times_sorted[i]/rint,
-            (((timessq_sorted[i]-times_sorted[i]*times_sorted[i]/rint)/rint) as f64).sqrt()); 
-        } 
+        report(&times,&timessq,names,repeats as f64)
+    }
+}
+
+/// Repeated tests on vectors of vectors of magnitudes steps in length, e.g. 10,100,1000  
+/// Time n runs of listed closures, identified by names,
+/// on random data of type &[Vec<u64>] and rn = Rnum::newu64;
+/// identified by names listed in names.
+pub fn benchvvu64(
+    rn:Rnum,
+    points: usize, // number of Vecs in each Vec<Vec<u8>>
+    magnitudes:usize,
+    repeats:usize,
+    names:&[&str],
+    closures:&[fn(&[Vec<u64>])]) { // concrete type here
+
+    let algno = names.len();
+    let mut timer = DevTime::new_simple();
+
+    println!("\n{YL}Nanoseconds of {BL}{}{YL} algorithms, {BL}&[Vec<f64>]{YL} data, {BL}{}{YL} magnitudes, {BL}{}{YL} points, {BL}{}{YL} repeats:{UN}",
+        algno,points,magnitudes,repeats ); 
+
+    for mag in 1..magnitudes+1 {  
+        let d = 10_usize.pow(mag as u32);
+        println!("\nDimensions: {BL}{}{UN}\n",d); 
+        let mut times = vec![0_u128;algno];
+        let mut timessq = vec![0_u128;algno];
+        for _ in 0..repeats {
+            let data = rn.ranvv(d,points).getvvu64(); // closures concrete type 
+            for (i,closure) in closures.iter().enumerate() {
+                timer.start();
+                closure(&data);
+                timer.stop();
+                let this_time = timer.time_in_nanos().unwrap();
+                times[i] += this_time; 
+                timessq[i] += this_time*this_time; 
+            }
+        }   
+        report(&times,&timessq,names,repeats as f64)
     }
 }
